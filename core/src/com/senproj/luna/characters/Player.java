dc44f64dd0778f61d63f3c34b9f1f886ddafda92
Twitch572 <dude.dodiecat.dudet@gmail.com>
2017-10-12 23:59:43 -0700

    Finish up player movement and camera translations

--- core/src/com/senproj/luna/characters/Player.java
+++ core/src/com/senproj/luna/characters/Player.java
@@ -1,5 +1,7 @@
 package com.senproj.luna.characters;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -7,16 +9,24 @@ import com.senproj.luna.helpers.Settings;
 
 public class Player {
 
-    private Vector2 pos;
+    private float speed;
+    private Vector2 pos, vel;
     private Texture tex;
 
     public Player() {
         tex = new Texture("game/sprites/player/temp_sprite_player_front.png");
-        pos = new Vector2(Settings.SCREEN_WIDTH / 2.0f - tex.getWidth() / 2.0f, Settings.SCREEN_HEIGHT / 2.0f - tex.getHeight() / 2.0f);
+        pos = new Vector2(Settings.SCREEN_WIDTH / 2.0f, Settings.SCREEN_HEIGHT / 2.0f);
+        vel = new Vector2(0, 0);
+        speed = 150;
+    }
+
+    public void update(float dt) {
+        updateVelocity();
+        pos.add(vel.scl(dt * speed));
     }
 
     public void draw(SpriteBatch batch) {
-        batch.draw(tex, pos.x, pos.y);
+        batch.draw(tex, pos.x - tex.getWidth() / 2.0f, pos.y - tex.getHeight() / 2.0f);
     }
 
     public void dispose() {
@@ -25,5 +35,24 @@ public class Player {
 
     public Vector2 getPosition() {
         return pos;
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.vel = velocity;
+    }
+
+    private void updateVelocity() {
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+            vel.y = 1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.S))
+            vel.y = -1;
+        else
+            vel.y = 0;
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+            vel.x = -1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.D))
+            vel.x = 1;
+        else
+            vel.x = 0;
     }
 }
--- core/src/com/senproj/luna/states/PlayState.java
+++ core/src/com/senproj/luna/states/PlayState.java
@@ -1,8 +1,10 @@
 package com.senproj.luna.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.senproj.luna.characters.Player;
 import com.senproj.luna.helpers.Settings;
 
@@ -14,16 +16,20 @@ public class PlayState extends GameState {
         super(gsm);
         gamemap = new Texture("game/map/background/temp_ingamemap.png");
         player = new Player();
-        camera.setToOrtho(false);
+        camera.setToOrtho(false, Settings.SCREEN_WIDTH / 2.0f, Settings.SCREEN_HEIGHT / 2.0f);
     }
 
     @Override
     public void update(float dt) {
+        player.update(dt);
+        camera.position.set(player.getPosition(), camera.position.z);
+        camera.update();
         handleInput();
     }
 
     @Override
     public void render(SpriteBatch batch) {
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(gamemap, 0, 0, Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT);
         player.draw(batch);
