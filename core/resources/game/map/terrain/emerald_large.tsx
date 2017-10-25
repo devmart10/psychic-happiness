738ebb5956eb49f634e46c401f596a291fe3304d
Devon Martin <devmart10@gmail.com>
2017-10-24 23:37:57 -0700

    Cleanup

--- core/resources/game/map/terrain/emerald_large.tsx
+++ core/resources/game/map/terrain/emerald_large.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="emerald_large" tilewidth="32" tileheight="32" tilecount="6072" columns="88">
- <image source="emerald_large.png" width="2816" height="2208"/>
+ <image source="../_/emerald_large.png" width="2816" height="2208"/>
 </tileset>
--- core/src/com/senproj/luna/map/GameMap.java
+++ core/src/com/senproj/luna/map/GameMap.java
@@ -1,48 +1,28 @@
 package com.senproj.luna.map;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
 
 public class GameMap {
     private OrthographicCamera camera;
-    private Texture background;
-    private Texture sheet;
-    private Tree tree;
-    private Tree tree2;
 
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
     public GameMap(OrthographicCamera camera) {
-        background = new Texture("game/map/background/map1.png");
-        sheet = new Texture("game/map/terrain/terrain.png");
-
         map = new TmxMapLoader().load("game/map/terrain/my_map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         this.camera = camera;
-
-        tree = new Tree(sheet);
-        tree.setPos(new Vector2(20 * 32, 15 * 32));
-
-        tree2 = new Tree(sheet);
-        tree2.setPos(new Vector2(25 * 32, 15 * 32));
     }
 
-    // don't need anymore, but leaving it here in case we want to go back
-    public void draw(SpriteBatch batch) {
-        batch.draw(background, 10*32, 0, 1024, 1024);
-        tree.draw(batch);
-        tree2.draw(batch);
+    public TiledMap getMap() {
+        return map;
     }
 
     public void dispose() {
-        background.dispose();
-        sheet.dispose();
+
     }
 
     public void render() {
